b7895e9ee9a37b2495503ba23812a01a95d7648d
matthewberzinskas <pasomatt@gmail.com>
2022-05-03 15:38:06 -0700

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div classname="App">
+      <div className="container border p-2">
+        <header className="text-center p-2 border">
+          React News
+        </header>
+        <div className="main border p-2">
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
