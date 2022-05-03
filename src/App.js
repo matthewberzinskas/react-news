0823632c03007cedcc9aa7ac39901b80ffa4c9bb
matthewberzinskas <pasomatt@gmail.com>
2022-05-03 16:28:00 -0700

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import NewsFeed from './components/NewsFeed';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <div className="container border p-2">
-        <header className="text-center p-2 border">
-          React News
-        </header>
+    <div className="App bg-dark min-vh-100">
+      <div className="container border p-2 bg-light">
+        <Navbar/>
         <div className="main border p-2">
         <NewsFeed/>
         </div>
---
+++ src/components/Navbar.js
@@ -0,0 +1,58 @@
+export default function Navbar () {
+
+  return (
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+    <div class="container-fluid">
+      
+
+      <a href="#" class="navbar-brand">Brand</a>
+      
+     <button
+        type="button"
+        class="navbar-toggler"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarCollapse"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+
+      <div
+        class="collapse navbar-collapse justify-content-between"
+        id="navbarCollapse"
+      >
+        <div class="navbar-nav">
+          <a href="#" class="nav-item nav-link active">Home</a>
+          <a href="#" class="nav-item nav-link">Profile</a>
+          
+
+          <div class="nav-item dropdown">
+            <a
+              href="#"
+              class="nav-link dropdown-toggle"
+              data-bs-toggle="dropdown"
+              >Messages</a
+            >
+            <div class="dropdown-menu">
+              <a href="#" class="dropdown-item">Inbox</a>
+              <a href="#" class="dropdown-item">Sent</a>
+              <a href="#" class="dropdown-item">Drafts</a>
+            </div>
+          </div>
+        </div>
+
+
+        <form class="d-flex ms-auto">
+          <div class="input-group">
+            <input type="text" class="form-control" placeholder="Search" />
+            <button type="button" class="btn btn-secondary">
+              <i class="bi-search"></i>
+            </button>
+          </div>
+        </form>
+
+      </div>
+    </div>
+  </nav>
+  )
+}
